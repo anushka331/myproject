bc21f30b90d68da1330daf7865a8e99d9379eb69
anushka331 <jadhavanu234@gmail.com>
2026-01-09 18:44:48 +0530

    added reactpractice vite project

---
+++ ReactPractice/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello React App</h1>
+      <p>Welcome to my first react application</p>
+    </div>
+        
+  )
+}
+
+export default App
